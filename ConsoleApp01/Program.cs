0f2fff5a091422c8072b4189918128b68488878a
Antracit1 <antuan4ik@gmail.com>
2022-01-10 22:20:44 +0100

    Add project files.

---
+++ ConsoleApp01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstApplication.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, What is your name?");
+			string name = Console.ReadLine();
+			Console.WriteLine("Hello, {0}", name);
+			Console.WriteLine("Hey,{0} where you from?", name);
+			string town = Console.ReadLine();
+			Console.WriteLine("Hello {0} from {1}", name, town);
+			Console.WriteLine("{0}, at this moment you in {1}?", name, town);
+			Console.ReadLine();
+
+		}
+	}
+}
---
+++ ConsoleAppDebugging/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirstApplication.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string MyName;
+			MyName = "Anton";
+			Console.WriteLine(MyName);
+			Console.ReadKey();
+
+
+
+		}
+
+	}
+}

0df25d929ca4872ed9fe1567b956ebac24daf8a0
Antracit1 <antuan4ik@gmail.com>
2022-01-15 00:12:14 +0100

    3,3  переменные с обозначением типов данных

--- ConsoleAppDebugging/Program.cs
+++ ConsoleAppDebugging/Program.cs
@@ -6,9 +6,17 @@ namespace FirstApplication.ConsoleApp
 	{
 		static void Main(string[] args)
 		{
-			string MyName;
-			MyName = "Anton";
-			Console.WriteLine(MyName);
+			const string MyName = "Anton";
+            ushort age = 27;
+			bool pet = false;
+			double foot = 43;
+
+
+			Console.WriteLine("Hello, my name is " + MyName);
+			Console.WriteLine("I'm " + age);
+            Console.WriteLine("Do I have a pet?" + pet);
+			Console.WriteLine("My shoe size is" + foot);
+
 			Console.ReadKey();
 
 
